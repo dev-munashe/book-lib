4a5a60612a808b23cc4be904892b1469ddb165be
Brighton Tandabantu <brighton.tandabantu@fullstack.co.za>
2023-06-05 10:55:32 +0200

    added the sign up.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import './App.css';
+import { useState, useEffect } from 'react'
+import SignUpLogin from './components/SignUp'
 
 function App() {
+
+  const [books, setBooks] = useState('');
+  const [IsSignedUp, setIsSignedUp] = useState(false);
+
+
+  useEffect(() => {
+    fetchBooks();
+  }, []);
+
+  const fetchBooks = async () => {
+    try {
+      const response = await fetch('https://localhost:7225/api/BooksLibraries/1');
+      const data = await response.json();
+      setBooks(data);
+    } catch (error) {
+      console.error(error);
+      console.log(`Failed because of error: ${error}`);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Okay lets build a book library.</h1>
-      </header>
+      <SignUpLogin IsSignedUp={IsSignedUp} />
+      <pre>{books.book}</pre>
     </div>
   );
 }
